657f1142743fd0ca8bcdbaac7c0ce816e0dd152c
azepelayo <Aze.Pelayo@mrm.com>
2021-09-07 18:50:44 +0800

    Conditional rendering updates - Added new content to be rendered when switched to transfer transaction type - New states for transfer transaction type - Fixed category field not switching to "" when transaction type is changed - Added clearInputs function to clear all inputs after submitting a transaction, so initialState variable was created - Added comments for better readabiility

--- src/App.js
+++ src/App.js
@@ -1,49 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
 function App() {
+
+  // Props to be passed down
   const [ accounts ] = useState(['cash', 'bank']);
   const [ transaction, setTransaction] = useState({
     month: "",
     item: {}
   });
-  const [ values, setValues] = useState({
-    transactionType: "income", 
-    transactionDate: new Date(),
-    account: "",
-    transactionCategory: "",
-    transactionAmount: 0,
-    transactionNotes:""
-  });
   const [ categories ] = useState({
     income: ['salary', 'interest'],
     expense: ['food', 'transportation']
   });
+
+  // States inside addEntry
+  const initialState = {
+    transactionType: "income", 
+    transactionDate: new Date(),
+    fromAccount: "",
+    toAccount: "",
+    transactionCategory: "",
+    transactionAmount: 0,
+    transactionNotes:""
+  }
+  const [ values, setValues] = useState(initialState);
   const [ errorMsgs, setErrorMsgs] = useState({
-    account: "",
+    fromAccount: "",
+    toAccount: "",
     transactionCategory: "",
     transactionAmount: ""
   });
+
+  // Handler Functions
+
+  useEffect(() => {
+    // Whenever we switch between transaction types, always reset transactionCategory
+    setValues( prevValues => ({...prevValues, transactionCategory : ""}));
+  }, [values.transactionType])
 
   const handleValueChange = (event) => {
     let {name, value}  = event.target;
     if(name === "transactionDate") {
       value = new Date(value)
     }
-    setValues({...values, [name] : value})
+    setValues({...values, [name] : value});
+  }
+
+  // Block invalid characters in number input field
+  const blockInvalidCharacter = (event) => {
+    ['e', 'E', '+', '-'].includes(event.key) && event.preventDefault();
   }
   
   const handleValidation = () => {
     let isFormValid = true;
-    if(values.account === "") {
+
+    if(values.fromAccount === "") {
       isFormValid = false;
-      setErrorMsgs( prevState => ({ ...prevState, account: "cannot be empty"}))
+      setErrorMsgs( prevState => ({ ...prevState, fromAccount: "cannot be empty"}))
     }
     else {
-      setErrorMsgs( prevState => ({ ...prevState, account: ""}))
+      setErrorMsgs( prevState => ({ ...prevState, fromAccount: ""}))
     }
 
-    if(values.transactionCategory === "") {
+    if(values.transactionType === "transfer" && values.toAccount === "") {
+      isFormValid = false;
+      setErrorMsgs( prevState => ({ ...prevState, toAccount: "cannot be empty"}))
+    }
+    else {
+      setErrorMsgs( prevState => ({ ...prevState, toAccount: ""}))
+    }
+
+    if(values.transactionType !== "transfer" && values.transactionCategory === "") {
       isFormValid = false;
       setErrorMsgs( prevState => ({ ...prevState, transactionCategory: "cannot be empty"}))
     }
@@ -62,6 +90,9 @@ function App() {
     return isFormValid;
   }
 
+  const clearInputs = () => {
+    setValues(initialState);
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -75,48 +106,51 @@ function App() {
         month: month,
         item: values
       })
+
+      clearInputs();
     }
   }
 
-  const blockInvalidCharacter = (event) => {
-    ['e', 'E', '+', '-'].includes(event.key) && event.preventDefault();
-  }
-
- 
   return (
     <div>
       <form onSubmit = {handleSubmit}>
+
+      {/* Select transaction type */}
         <label>
-        <input 
-            type = "radio"
-            name = "transactionType"
-            value = "income"
-            checked = {values.transactionType === "income"}
-            onChange = {handleValueChange}
-        />
+          <input 
+              type = "radio"
+              name = "transactionType"
+              value = "income"
+              checked = {values.transactionType === "income"}
+              onChange = {handleValueChange}
+          />
           Income
         </label>
+
         <label>
-        <input 
-            type = 'radio'
-            name = 'transactionType'
-            value = 'expense'
-            checked = {values.transactionType === "expense"}
-            onChange = {handleValueChange}
-        />
+          <input 
+              type = 'radio'
+              name = 'transactionType'
+              value = 'expense'
+              checked = {values.transactionType === "expense"}
+              onChange = {handleValueChange}
+          />
           Expense
         </label>
+
         <label>
-        <input 
-            type = 'radio'
-            name = 'transactionType'
-            value = 'transfer'
-            checked = {values.transactionType === "transfer"}
-            onChange = {handleValueChange}
-        />
+          <input 
+              type = 'radio'
+              name = 'transactionType'
+              value = 'transfer'
+              checked = {values.transactionType === "transfer"}
+              onChange = {handleValueChange}
+          />
           Transfer
         </label>
         <br/>
+
+        {/* Transaction information */}
         <label>
           Transaction Date: 
           <input 
@@ -127,56 +161,109 @@ function App() {
           />
         </label>
         <br/>
-        <label>
-          Account:
-          <select 
-            value = {values.account}
-            onChange = {handleValueChange}
-            name = "account"
-          >
-           <option value = "" disabled>-- Select one --</option>
-            {accounts.map((item, key) => {
-              return (
-                <option value = {item} key = {key}>
-                  {item}
-                </option>
-              )
-            })}
-          </select>
-          <span style={{color: "red"}}>{errorMsgs.account}</span>
-        </label>
-        <br/>
-        <label>
-          Category:
-          <select 
-            value = {values.transactionCategory}
-            onChange = {handleValueChange}
-            name = "transactionCategory"
-          >
-            <option value = "" disabled>-- Select one --</option>
-            {categories[values.transactionType].map((item, key) => {
-              return (
-                <option value = {item} key = {key}>
-                  {item}
-                </option>
-              )
-            })}
-          </select>
-          <span style={{color: "red"}}>{errorMsgs.transactionCategory}</span>
-        </label>
-        <br/>
+
+        {/* For Income and Expense transaction types */}
+        {(values.transactionType !== "transfer") &&
+          <div>
+            <label>
+              Account:
+              <select 
+                value = {values.fromAccount}
+                onChange = {handleValueChange}
+                name = "fromAccount"
+              >
+              <option value = "" disabled>-- Select one --</option>
+                {accounts.map((item, key) => {
+                  return (
+                    <option value = {item} key = {key}>
+                      {item}
+                    </option>
+                  )
+                })}
+              </select>
+              <span style={{color: "red"}}>{errorMsgs.fromAccount}</span>
+            </label>
+            <br/>
+
+            <label>
+              Category:
+              <select 
+                value = {values.transactionCategory}
+                onChange = {handleValueChange}
+                name = "transactionCategory"
+              >
+                <option value = "" disabled>-- Select one --</option>
+                {categories[values.transactionType].map((item, key) => {
+                  return (
+                    <option value = {item} key = {key}>
+                      {item}
+                    </option>
+                  )
+                })}
+              </select>
+              <span style={{color: "red"}}>{errorMsgs.transactionCategory}</span>
+            </label>
+            <br/>
+          </div>
+        }
+        
+        {/* For transfer transaction type */}
+        {(values.transactionType === "transfer") &&
+          <div>
+            <label>
+              From: 
+              <select 
+                value = {values.fromAccount}
+                onChange = {handleValueChange}
+                name = "fromAccount"
+              >
+              <option value = "" disabled>-- Select one --</option>
+                {accounts.map((item, key) => {
+                  return (
+                    <option value = {item} key = {key}>
+                      {item}
+                    </option>
+                  )
+                })}
+              </select>
+              <span style={{color: "red"}}>{errorMsgs.fromAccount}</span>
+            </label>
+            <br/>
+
+            <label>
+              To:
+              <select 
+                value = {values.toAccount}
+                onChange = {handleValueChange}
+                name = "toAccount"
+              >
+              <option value = "" disabled>-- Select one --</option>
+                {accounts.map((item, key) => {
+                  return (
+                    <option value = {item} key = {key}>
+                      {item}
+                    </option>
+                  )
+                })}
+              </select>
+              <span style={{color: "red"}}>{errorMsgs.toAccount}</span>
+            </label>
+            <br/>
+          </div>
+        }
+
         <label>
           Amount:
           <input 
             type = "number"
             name = "transactionAmount"
             onChange = {handleValueChange}
+            value = {values.transactionAmount}
             min = "0"
             placeholder = "0"
             onKeyDown = {blockInvalidCharacter}
           />
           <span style={{color: "red"}}>{errorMsgs.transactionAmount}</span>
-
         </label>
         <br/>
         <label>
@@ -195,7 +282,18 @@ function App() {
       <p>Month: {transaction.month} </p>
       <p>Date: {moment(transaction.item.transactionDate).format('LL')} </p>
       <p>Category: {transaction.item.transactionCategory}</p>
-      <p>Account: {transaction.item.account}</p>
+
+      {
+        values.transactionType !== "transfer" &&
+        <p>Account: {transaction.item.fromAccount}</p> 
+      }
+      {
+        values.transactionType === "transfer" &&
+        <div>
+          <p>From: {transaction.item.fromAccount}</p> 
+          <p>To: {transaction.item.toAccount}</p> 
+        </div>
+      }
       <p>Amount: {transaction.item.transactionAmount}</p>
       <p>Notes: {transaction.item.transactionNotes}</p>
     </div>
