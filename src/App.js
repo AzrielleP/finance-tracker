65de9d1cc4b1229590c1ce947aad632db6ab4014
azepelayo <Aze.Pelayo@mrm.com>
2021-09-08 16:24:23 +0800

    Fixed rendering issues Renamed files, added comments, and applied Prettier

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import AddEntry from './components/addEntry';
-import Transaction from './components/Transaction';
-
+import React, { useState, useEffect, useRef } from "react";
+import AddEntry from "./components/AddEntry";
+import Transaction from "./components/Transaction";
 
 function App() {
+    // Props to be passed down
+    const [accounts] = useState(["cash", "bank"]);
+    const [data, setData] = useState([]);
+    const [transaction, setTransaction] = useState({});
+    const [categories] = useState({
+        income: ["salary", "interest"],
+        expense: ["food", "transportation"],
+    });
 
-  // Props to be passed down
-  const [ accounts ] = useState(['cash', 'bank']);
-  const [ data, setData ] = useState([]);
-  const [ transaction, setTransaction] = useState({});
-  const [ categories ] = useState({
-    income: ['salary', 'interest'],
-    expense: ['food', 'transportation']
-  });
+    const handleTransaction = (value) => {
+        setTransaction(value);
+    };
 
-  const handleTransaction = (value) => {
-    setTransaction(value);
-  }
+    // Do not run useEffect on first render
+    const firstUpdate = useRef(true);
+    useEffect(() => {
+        const handleDataUpdates = () => {
+            setData((prevData) => [...prevData, transaction]);
+        };
 
-  const handleDataUpdates = (value) => {
-    setData( prevData => [...prevData, value]);
-  }
+        if (firstUpdate.current) {
+            firstUpdate.current = false;
+            return;
+        }
+        handleDataUpdates();
+    }, [transaction]);
 
-  useEffect(() => {
-    console.log(data)
-    handleDataUpdates();
-  }, [transaction])
-
-  return (
-    <div>
-      <AddEntry
-        accounts = {accounts}
-        transaction = {transaction}
-        categories = {categories}
-        handleTransaction = {handleTransaction}
-      />
-      <Transaction
-        data = {data}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <AddEntry
+                accounts={accounts}
+                transaction={transaction}
+                categories={categories}
+                handleTransaction={handleTransaction}
+            />
+            <Transaction data={data} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,38 +1,32 @@
-import React from 'react';
-import moment from 'moment';
+import React from "react";
+import moment from "moment";
 
 function Transaction(props) {
-    const { data } = props
+    const { data } = props;
 
     return (
         <div>
-             <h1>Recent Transactions</h1>
-            {
-                data.length !== 0 && data.map((item, key) => {
+            <h1>Recent Transactions</h1>
+            {data.map((item, key) => {
                 return (
-                    <div key = {key}>
-                    <p>Date: {moment(item.transactionDate).format('LL')} </p>
-                    <p>Category: {item.transactionCategory}</p>
+                    <div key={key}>
+                        <p>Date: {moment(item.transactionDate).format("LL")} </p>
+                        <p>Category: {item.transactionCategory}</p>
 
-                    {
-                        item.transactionType !== "transfer" &&
-                        <p>Account: {item.fromAccount}</p> 
-                    }
-                    {
-                        item.transactionType === "transfer" &&
-                        <div>
-                        <p>From: {item.fromAccount}</p> 
-                        <p>To: {item.toAccount}</p> 
-                        </div>
-                    }
-                    <p>Amount: {item.transactionAmount}</p>
-                    <p>Notes: {item.transactionNotes}</p>
+                        {item.transactionType !== "transfer" && <p>Account: {item.fromAccount}</p>}
+                        {item.transactionType === "transfer" && (
+                            <div>
+                                <p>From: {item.fromAccount}</p>
+                                <p>To: {item.toAccount}</p>
+                            </div>
+                        )}
+                        <p>Amount: {item.transactionAmount}</p>
+                        <p>Notes: {item.transactionNotes}</p>
                     </div>
-                )
-                })
-            }
+                );
+            })}
         </div>
-    )
+    );
 }
 
 export default Transaction;
